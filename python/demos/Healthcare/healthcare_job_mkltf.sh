cc2853af5ef97da18f06ab0f017dbbdcaf8e8bc4
Keval Vora <kevalvora123@gmail.com>
2020-01-25 01:57:54 +0530

    Added support for Selecting Conda Env in the No-OpenVino NB

--- python/demos/Healthcare/healthcare_job_mkltf.sh
+++ python/demos/Healthcare/healthcare_job_mkltf.sh
@@ -1,13 +1,4 @@
-
 cd $PBS_O_WORKDIR
-
-mkltf_exist=/data/software/miniconda3/4.7.12/bin/conda info --envs | grep "mkltf "
-
-if [ -z "$mkltf_exist" ]
-then
-    /data/software/miniconda3/4.7.12/bin/conda config --set auto_activate_base false
-    /data/software/miniconda3/4.7.12/bin/conda env create -f "mkltf.yml"
-fi
 
 # Running the code inside mkltf conda env
 SAMPLEPATH=$PBS_O_WORKDIR
--- python/demos/Healthcare/healthcare_job_stocktf.sh
+++ python/demos/Healthcare/healthcare_job_stocktf.sh
@@ -1,14 +1,5 @@
-
 cd $PBS_O_WORKDIR
-echo $PBS_O_WORKDIR
-stocktf_exist=/data/software/miniconda3/4.7.12/bin/conda info --envs | grep "stocktf "
-echo $stocktf_exist
 
-if [ -z "$stocktf_exist" ]
-then
-    /data/software/miniconda3/4.7.12/bin/conda config --set auto_activate_base false
-    /data/software/miniconda3/4.7.12/bin/conda env create -f "stocktf.yml"
-fi
 
 # Running the code inside stocktf conda env
 SAMPLEPATH=$PBS_O_WORKDIR
