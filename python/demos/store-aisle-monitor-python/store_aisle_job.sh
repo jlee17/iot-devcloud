37d61d96a332aabbc8de29830793e4de44bae9eb
Keval Vora <kevalvora123@gmail.com>
2020-01-27 23:42:17 +0530

    Added demo for sales of store aisle monitor

---
+++ python/demos/store-aisle-monitor-python/store_aisle_job.sh
@@ -0,0 +1,33 @@
+
+#The default path for the job is your home directory, so we change directory to where the files are.
+cd $PBS_O_WORKDIR
+
+#store_aisle_job script writes output to a file inside a directory. We make sure that this directory exists.
+#The output directory is the first argument of the bash script
+mkdir -p $1
+OUTPUT_FILE=$1
+DEVICE=$2
+FP_MODEL=$3
+INPUT_FILE=$4
+THRESHOLD=$5
+
+if [ $DEVICE = "HETERO:FPGA,CPU" ]; then
+    #Environment variables and compilation for edge compute nodes with FPGAs
+    source /opt/intel/init_openvino.sh
+    aocl program acl0 /opt/intel/openvino/bitstreams/a10_vision_design_sg1_bitstreams/2019R3_PV_PL1_FP11_MobileNet_TinyYolo_Clamp.aocx
+fi
+
+SAMPLEPATH=${PBS_O_WORKDIR}
+if [ "$FP_MODEL" = "FP32" ]; then
+  MODELPATH=${SAMPLEPATH}/models/intel/person-detection-retail-0013/FP32/person-detection-retail-0013.xml
+else
+  MODELPATH=${SAMPLEPATH}/models/intel/person-detection-retail-0013/FP16/person-detection-retail-0013.xml
+fi
+
+#Running the store aisle code
+python3 store_aisle_monitor.py  -m ${MODELPATH} \
+                                -i ${INPUT_FILE} \
+                                -o ${OUTPUT_FILE} \
+                                -d ${DEVICE} \
+                                -pt ${THRESHOLD}\
+                                -l /opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_sse4.so
