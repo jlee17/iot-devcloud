1bb80c559a661527a516e8907932ad10814cbd6f
u22646 <u22646@s099-n003.devcloud-edge>
2020-03-06 16:33:47 -0800

    fixed object detection cpp

---
+++ cpp/object_detection/common/format_reader/CMakeFiles/format_reader.dir/DependInfo.cmake
@@ -0,0 +1,35 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/MnistUbyte.cpp" "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/CMakeFiles/format_reader.dir/MnistUbyte.cpp.o"
+  "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/bmp.cpp" "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/CMakeFiles/format_reader.dir/bmp.cpp.o"
+  "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/format_reader.cpp" "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/CMakeFiles/format_reader.dir/format_reader.cpp.o"
+  "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/opencv_wraper.cpp" "/home/u22646/cpp-verify/iot-devcloud/cpp/object_detection/common/format_reader/CMakeFiles/format_reader.dir/opencv_wraper.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Preprocessor definitions for this target.
+set(CMAKE_TARGET_DEFINITIONS_CXX
+  "IMPLEMENT_FORMAT_READER"
+  "USE_OPENCV"
+  "format_reader_EXPORTS"
+  )
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/opt/intel/openvino/deployment_tools/inference_engine/include"
+  "common"
+  "common/format_reader"
+  "common/format_reader/.."
+  "/opt/intel/openvino_2020.1.023/opencv/include"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ cpp/object_detection/common/format_reader/CMakeFiles/format_reader.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "../../build/intel64/libformat_reader.pdb"
+  "../../build/intel64/libformat_reader.so"
+  "CMakeFiles/format_reader.dir/MnistUbyte.cpp.o"
+  "CMakeFiles/format_reader.dir/bmp.cpp.o"
+  "CMakeFiles/format_reader.dir/format_reader.cpp.o"
+  "CMakeFiles/format_reader.dir/opencv_wraper.cpp.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/format_reader.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
