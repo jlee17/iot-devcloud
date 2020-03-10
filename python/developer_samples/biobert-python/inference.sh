a13557ddfa1f4293bb0c78b7639791e657adecf1
Jason Lee <jason.lee@intel.com>
2020-03-10 16:24:00 -0700

    Add BioBERT notebook

---
+++ python/developer_samples/biobert-python/inference.sh
@@ -0,0 +1,16 @@
+
+cd $PBS_O_WORKDIR
+
+mkdir -p $1
+OUTPUT_DIR=$1
+DEVICE=$2
+
+if [ "$DEVICE" = "HETERO:FPGA,CPU" ]; then
+    # Environment variables and compilation for edge compute nodes with FPGAs - Updated for OpenVINO 2020.1
+    source /opt/altera/aocl-pro-rte/aclrte-linux64/init_opencl.sh
+    export LD_LIBRARY_PATH=${LD_LIBRARY_PATH}:/opt/intel/openvino/bitstreams/a10_vision_design_sg1_bitstreams/BSP/a10_1150_sg1/linux64/lib
+    aocl program acl0 /opt/intel/openvino/bitstreams/a10_vision_design_sg1_bitstreams/2019R4_PL1_FP16_MobileNet_Clamp.aocx
+    export CL_CONTEXT_COMPILER_MODE_INTELFPGA=3
+fi
+
+python3 python/inference.py -d ${DEVICE} -o ${OUTPUT_DIR}
